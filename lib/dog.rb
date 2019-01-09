a652c834ba34315792c20b5d4aaba3a75ee679b0
Sarah Paz <sarah.paz@gmail.com>
2019-01-09 05:02:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,14 @@
 class Dog
   @@all = []
-  
+
   def initialize(name)
     @name = name
     @@all << self
   end
 
   def self.all
-    @all.each do {|dog_name| puts "#{dog_name}"}
+    @all.each do |dog_name|
+      puts "#{dog_name}"
+    end
   end
 end
